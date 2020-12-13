a355507f0af4ad18a29b2151daf823670b935364
Entaro Adun <dcwmark@gmail.com>
2020-12-13 01:35:53 -0500

    Basic mfe apps

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,57 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import dotenv from 'dotenv';
+
+import MicroFrontend from './MicroFrontend';
+import Navbar from './components/Navbar';
+
 import './App.css';
+
+dotenv.config();
+
+const defaultHistory = createBrowserHistory();
+
+const {
+  REACT_APP_MFE_A: mfeAHost,
+  REACT_APP_MFE_B: mfeBHost,
+  REACT_APP_MFE_C: mfeCHost,
+} = process.env;
+
+console.log(`***** env::${JSON.stringify(process.env)}`);
+console.log(`***** mfeAHost::${mfeAHost}`);
+console.log(`***** mfeBHost::${mfeBHost}`);
+console.log(`***** mfeCHost::${mfeCHost}`);
+
+const mfeA = ({ history = defaultHistory }) => {
+  return <MicroFrontend history={ history } host={ mfeAHost } name="mfeA" />
+};
+
+const mfeB = ({ history = defaultHistory }) => {
+  return <MicroFrontend history={ history } host={ mfeBHost } name="mfeB" />
+};
+
+const mfeC = ({ history = defaultHistory }) => {
+  return <MicroFrontend history={ history } host={ mfeCHost } name="mfeC" />
+};
+
+const HOME = () => {
+  return (
+    <h1 className="app-head">MFE Home</h1>
+  )
+};
 
 const App = () => {
   return (
     <>
-      <h1 className="app-head">MFE</h1>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={ HOME } />
+          <Route exact path="/mfe-a" component={ mfeA } />
+          <Route exact path="/mfe-b" component={ mfeB } />
+          <Route exact path="/mfe-c" component={ mfeC } />
+        </div>
+      </BrowserRouter>
     </>
   );
 }
---
+++ container/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { Link, NavLink } from 'react-router-dom';
+
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link className="navbar-brand" to="/"><h2>MFE</h2></Link>
+        <ul className="navbar-nav">
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to="/mfe-a">mfe-a</NavLink></li>
+          <li><NavLink to="/mfe-b">mfe-b</NavLink></li>
+          <li><NavLink to="/mfe-c">mfe-c</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
